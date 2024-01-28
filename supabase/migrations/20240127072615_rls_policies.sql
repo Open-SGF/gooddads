7275f43a34bcfc134e275b5b36c97ac81abb9fee
Nathan Toombs <nathan.toombs@congenius.com>
2024-01-28 16:38:05 -0600

    Add tests for RLS policies

--- supabase/migrations/20240127072615_rls_policies.sql
+++ supabase/migrations/20240127072615_rls_policies.sql
@@ -11,11 +11,11 @@ CREATE POLICY "All users can view their own profile." ON users FOR SELECT TO aut
 DROP POLICY IF EXISTS "All users can update their own profiles." ON users;
 CREATE POLICY "All users can update their own profiles." ON users FOR UPDATE USING (auth.uid() = id);
 
+DROP POLICY IF EXISTS "Admin users can view all profiles." ON users;
+CREATE POLICY "Admin users can view all profiles." ON users FOR SELECT USING (get_my_claim('userrole'::text) = '"ADMIN"'::jsonb);
+
 DROP POLICY IF EXISTS "Admin users can update any profile." ON users;
 CREATE POLICY "Admin users can update any profile." ON users FOR UPDATE USING (get_my_claim('userrole'::text) = '"ADMIN"'::jsonb);
 
-DROP POLICY IF EXISTS "Admin users can view any profile" ON users;
-CREATE POLICY "Admin users can view any profile" ON users FOR SELECT USING (get_my_claim('userrole'::text) = '"ADMIN"'::jsonb);
-
 DROP POLICY IF EXISTS "Intake users can create new profiles." ON users;
-CREATE POLICY "Intake users can create new profiles." ON users FOR INSERT USING (get_my_claim('userrole'::text) = '"INTAKE"'::jsonb);
+CREATE POLICY "Intake users can create new profiles." ON users FOR INSERT WITH CHECK (get_my_claim('userrole'::text) = '"INTAKE"'::jsonb);
