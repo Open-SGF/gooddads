7cbec3f7f4e910d20577f5ef44ad0b12ac9817db
jugglingdev <kaylamarie1785@gmail.com>
2024-07-02 19:25:30 -0500

    chore: create models, factories, and seeders based on migrations
    Example command: php artisan make:model SingularModelName -f -s

---
+++ database/seeders/ProgramAssignmentSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProgramAssignmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
