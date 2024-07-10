51d55850c896edb6335370ce0b729101bc610941
jugglingdev <kaylamarie1785@gmail.com>
2024-07-09 20:44:33 -0500

    chore: update child model

--- app/Models/Child.php
+++ app/Models/Child.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Child extends Model
-{
-    use HasFactory;
+/**
+ * @property string $id
+ * @property string $dad_id
+ * @property string $name
+ * @property \DateTime $date_of_birth
+ * @property string $contact
+ * @property float $child_support
+ * @property \App\Models\Dad $dad
+ */
+
+class Child extends Model {
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'dad_id',
+        'name',
+        'date_of_birth',
+        'contact',
+        'child_support',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    /**
+     * Define the relationship to the Dad model.
+     *
+     * @return BelongsTo<Dad, self>
+     */
+    public function dad(): BelongsTo {
+        return $this->belongsTo(Dad::class, 'dad_id', 'id');
+    }
 }
