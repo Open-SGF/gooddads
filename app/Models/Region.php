42bb057e374e467e1c788de6471bf0c333557302
jugglingdev <kaylamarie1785@gmail.com>
2024-07-09 20:43:44 -0500

    chore: update region model

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
-class Region extends Model
-{
-    use HasFactory;
+/**
+ * @property string $id
+ * @property string $description
+ */
+
+class Region extends Model {
+    use HasFactory, HasUuids;
+    
+    protected $fillable = [
+        'description',
+    ];
+    
+    protected $keyType = 'string';
+    public $incrementing = false;
 }
