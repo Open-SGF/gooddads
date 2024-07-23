ee986d92372ba43396d2b7fb6ca808c2eee4f30b
abem93 <aimejia4@gmail.com>
2024-07-23 04:39:32 -0500

    chore: update module model

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Module extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $fillable = 
+    [
+        'description',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function program(): BelongsTo
+    {
+        return $this->belongsTo(Program::class);
+    }
+
+    public function moduleAssignments(): HasMany
+    {
+        return $this->hasMany(ModuleAssignment::class);
+    }
+    public function quizzes(): HasMany
+    {
+        return $this->hasMany(Quiz::class);
+    }
 }
