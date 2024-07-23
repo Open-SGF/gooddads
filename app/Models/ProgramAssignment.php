309ab926d88b71d62669e2b0c9689ab7fa8dfb82
abem93 <aimejia4@gmail.com>
2024-07-23 04:39:10 -0500

    chore: update program assigment model

--- app/Models/ProgramAssignment.php
+++ app/Models/ProgramAssignment.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProgramAssignment extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'start_date',
+        'completed',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+
+    protected $casts = [
+        'start_date' => 'immutable_datetime',
+        'completed' => 'boolean',
+    ];
+
+    public function dadClass(): BelongsTo
+    {
+        return $this->belongsTo(DadClass::class);
+    }
 }
