e8da9bbc96b78d90ce08af9c85b5d60dbd9edc16
abem93 <aimejia4@gmail.com>
2024-07-23 04:42:45 -0500

    chore: update quiz question model

--- app/Models/QuizQuestion.php
+++ app/Models/QuizQuestion.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
+use App\Enums\QuizQuestionType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class QuizQuestion extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'question',
+    ];
+
+    protected $casts = [
+        'type' => QuizQuestionType::class,
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+
+    public function quizQuestionOptions(): HasMany
+    {
+        return $this->hasMany(QuizQuestionOption::class);
+    }
 }
