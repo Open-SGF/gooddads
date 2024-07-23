063c0b322fcfd6d77815ab643e0eb987dcf126fe
abem93 <aimejia4@gmail.com>
2024-07-23 04:38:00 -0500

    chore: update program model

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Program extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'description', 
+        'length',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
 }
+
