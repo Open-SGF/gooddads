11e84cd90270b132c4f352467ddec376ca41dddd
abem93 <aimejia4@gmail.com>
2024-07-24 00:34:51 -0500

    chore: Update Dad Class model

--- app/Models/DadClass.php
+++ app/Models/DadClass.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DadClass extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    /**
+     * Define the relationship to the Region model.
+     *
+     * @return BelongsTo<Region, self>
+     */
+    public function region(): BelongsTo
+    {
+        return $this->belongsTo(Region::class);
+    }
 }
