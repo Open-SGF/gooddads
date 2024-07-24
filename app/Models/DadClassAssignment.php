5770ab4a5c0a2be0c9bad53a0c0d77928bcb2f3b
abem93 <aimejia4@gmail.com>
2024-07-24 00:39:22 -0500

    chore: Update Dad Class Assignment Model

--- app/Models/DadClassAssignment.php
+++ app/Models/DadClassAssignment.php
@@ -2,10 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DadClassAssignment extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    /**
+     * Define the relationship to the User model.
+     *
+     * @return BelongsTo<User, self>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Define the relationship to the DadClass model.
+     *
+     * @return BelongsTo<DadClass, self>
+     */
+    public function dadClass(): BelongsTo
+    {
+        return $this->belongsTo(DadClass::class);
+    }
 }
