f7cfa3cd43d951a7fb13ec69b08ff702a7637927
Abraham Mejia <aimejia4@gmail.com>
2024-09-02 21:50:54 -0500

    chore: updated blank models

--- app/Models/ResponsibleParty.php
+++ app/Models/ResponsibleParty.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $id
+ * @property string $phone_number
+ * @property string $role
+ */
 class ResponsibleParty extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'phone_number',
+        'role'
+    ];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
 }
