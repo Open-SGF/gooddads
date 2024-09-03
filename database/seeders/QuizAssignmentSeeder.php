9f8d4ba398a5c00700ae08c1f7a2238bf9d5c320
abem93 <aimejia4@gmail.com>
2024-09-03 18:48:59 -0500

    chore: update Quiz Assignment seeder

--- database/seeders/QuizAssignmentSeeder.php
+++ database/seeders/QuizAssignmentSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\QuizAssignment;
+use App\Models\QuizQuestion;
+use App\Models\QuizQuestionOption;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class QuizAssignmentSeeder extends Seeder
