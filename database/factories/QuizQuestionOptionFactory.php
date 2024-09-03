f4d37ec4a9fcd67249794e667165f6b80b0acf40
Abraham Mejia <aimejia4@gmail.com>
2024-09-02 21:54:17 -0500

    chore: update Quiz Question Option Factory

--- database/factories/QuizQuestionOptionFactory.php
+++ database/factories/QuizQuestionOptionFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\QuizQuestion;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,30 @@ class QuizQuestionOptionFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'id' => Str::uuid(),
+        'quiz_question_id' => QuizQuestion::factory(),
+            'answer' => fake()->sentence(),
+            'is_correct' => fake()->boolean(),
         ];
+    }
+
+     /**
+     * Indicate that the answer is Correct
+     */
+    public function correctResponse(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_correct' => true,
+        ]);
+    }
+
+    /**
+     * Indicate that the response is Incorrect
+     */
+    public function incorrectResponse(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_correct' => false,
+        ]);
     }
 }
