5852983aa478e0bf1aef02e311204ca7905848d6
abem93 <aimejia4@gmail.com>
2024-09-03 18:50:23 -0500

    chore: update  Quiz seeder

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Module;
+use App\Models\Quiz;
 use Illuminate\Database\Seeder;
 
 class QuizSeeder extends Seeder
@@ -11,6 +13,22 @@ class QuizSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Fetch existing Modules
+        $modules = Module::all();
+
+        // Ensure we have modules before creating quizzes
+        if ($modules->isEmpty()) {
+            $this->command->info('No modules found. Skipping Quiz creation.');
+
+            return;
+        }
+
+        // Create quizzes for each module
+        foreach ($modules as $module) {
+            Quiz::factory()
+                ->count(rand(1, 3))  // Create 1 to 3 quizzes per module
+                ->for($module)
+                ->create();
+        }
     }
 }
