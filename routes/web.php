f9ac0e7ec79e9abd78cac7c4d588f7b3f219816d
Nathan Toombs <nathan.toombs@congenius.com>
2024-10-21 22:49:34 -0500

    Fix bugs

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/admin', function () {
     return Inertia::render('Admin');
 })->middleware(['auth', 'verified', 'admin', 'role:admin'])->name('admin.index');
 
-Route::middleware('auth')->group(functifon () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
