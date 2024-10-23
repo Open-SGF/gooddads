4178a3498e476f9c2b090e589a45072ac294afc5
Nathan Toombs <nathan.toombs@congenius.com>
2024-10-22 19:46:18 -0500

    Add storybook and shadcn stories

---
+++ stories/shadcn/label.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Label } from "../../resources/js/Components/ui"
+
+/**
+ * Renders an accessible label associated with controls.
+ */
+const meta = {
+  title: "shadcn/Label",
+  component: Label,
+  tags: ["autodocs"],
+  argTypes: {
+    children: {
+      control: { type: "text" },
+    },
+  },
+  args: {
+    children: "Your email address",
+    htmlFor: "email",
+  },
+} satisfies Meta<typeof Label>
+
+export default meta
+
+type Story = StoryObj<typeof Label>
+
+/**
+ * The default form of the label.
+ */
+export const Default: Story = {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import tailwindAnimate from 'tailwindcss-animate'
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
@@ -61,10 +62,42 @@ export default {
     				'3': 'hsl(var(--chart-3))',
     				'4': 'hsl(var(--chart-4))',
     				'5': 'hsl(var(--chart-5))'
+    			},
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
     			}
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out'
     		}
     	}
     },
 
-    plugins: [forms, require("tailwindcss-animate")],
+    plugins: [forms, tailwindAnimate],
 };
