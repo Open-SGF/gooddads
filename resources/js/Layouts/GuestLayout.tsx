ae41c5f7eb58a42f16804e93b4258500d8000e2c
jugglingdev <kaylamarie1785@gmail.com>
2025-01-25 15:28:33 -0600

    fix: added top and bottom padding for styling on guest layout to accomodate larger children

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react'
 
 export default function Guest({ children }: PropsWithChildren) {
 	return (
-		<div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 px-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+		<div className="min-h-screen flex flex-col sm:justify-center items-center p-6 bg-gray-100 dark:bg-gray-900">
 			<div className="w-full sm:max-w-md px-12 py-14 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
 				<div className="mb-6">
 					<Link href="/" className="flex justify-center">
