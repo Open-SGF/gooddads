9e0f1a402201ab4a24b1b36252dcfa9e2921a46b
Kevin DeBrecht <99675851+kd-at-mi@users.noreply.github.com>
2025-02-01 20:05:53 -0600

    Participant Registration - Routes, Controllers, Forms, Test

--- resources/js/Pages/Intake/Index.tsx
+++ resources/js/Pages/Intake/Index.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import { Button, DataTable } from '@/Components/ui'
-import { PlusIcon } from '@radix-ui/react-icons'
+import { Button } from '@/Components/ui'
 import { Head, Link } from '@inertiajs/react'
-import { type PageProps, User } from '@/types'
+import { type PageProps } from '@/types'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
-import { usePermission } from '@/hooks/permissions'
-
-// IntakeIndex
 
 interface IntakeIndexProps extends PageProps {}
 
 export const IntakeIndex: React.FC<IntakeIndexProps> = ({ auth }) => {
-	const { hasPermission } = usePermission(auth.user)
-
 	return (
 		<AuthenticatedLayout
 			user={auth.user}
@@ -21,22 +15,14 @@ export const IntakeIndex: React.FC<IntakeIndexProps> = ({ auth }) => {
 					<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
 						Participant Registration
 					</h2>
-
-					{hasPermission('create users') && (
-						<Button size="sm" asChild>
-							<a href={route('users.create')}>
-								<PlusIcon /> Create User
-							</a>
-						</Button>
-					)}
 				</div>
 			}
 		>
-			<Head title="Users" />
+			<Head title="Register New Participant" />
 			<div className="py-12">
 				<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
 					<Button>
-						<Link href={route('users.create')}>Start New Registration</Link>
+						<Link href={route('users.create')}>Start Registration</Link>
 					</Button>
 				</div>
 			</div>
