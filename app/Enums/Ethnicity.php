bc0b8b73fa410573f0b1d87bdfc4808b096e93f1
Kevin DeBrecht <99675851+kd-at-mi@users.noreply.github.com>
2025-02-11 16:31:41 -0600

    Update MaritalStatus and Ethnicity enums.  Add EnumDisplayArray.php trait

--- app/Enums/Ethnicity.php
+++ app/Enums/Ethnicity.php
@@ -2,11 +2,24 @@
 
 namespace App\Enums;
 
+use App\Concerns\EnumDisplayArray;
+
 enum Ethnicity: string
 {
+    use EnumDisplayArray;
     case White = 'white';
-    case AfricanAmerican = 'africanAmerican';
-    case NativeAmerican = 'nativeAmerican';
+    case AfricanAmerican = 'african_american';
+    case NativeAmerican = 'native_american';
     case Asian = 'asian';
-    case PacificIslander = 'pacificIslander';
+    case PacificIslander = 'pacific_islander';
+    case Hispanic = 'hispanic';
+    case NoAnswer = 'no_answer';
+
+    public function displayValue(): string
+    {
+        return match ($this) {
+            self::NoAnswer => 'Prefer Not To Answer',
+            default => $this->defaultDisplayValue(),
+        };
+    }
 }
--- app/Enums/MaritalStatus.php
+++ app/Enums/MaritalStatus.php
@@ -2,8 +2,11 @@
 
 namespace App\Enums;
 
+use App\Concerns\EnumDisplayArray;
+
 enum MaritalStatus: string
 {
+    use EnumDisplayArray;
     case Single = 'single';
     case Married = 'married';
     case Divorced = 'divorced';
