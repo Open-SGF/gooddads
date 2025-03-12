99b062f47f49fde1fa6050bebee74a65edfbf53b
Kayla Paden <kaylamarie1785@gmail.com>
2025-03-11 21:14:34 -0500

    feat: implement routing for classes and curriculum tabs in navbar

--- resources/js/Pages/Classes/List.tsx
+++ resources/js/Pages/Classes/List.tsx
@@ -1,6 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
 import { Head } from '@inertiajs/react'
-import { FileChartColumn } from 'lucide-react'
+import { GraduationCap } from 'lucide-react'
 import { PageProps } from '@/types'
 
 export default function List({ auth }: PageProps) {
@@ -10,16 +10,16 @@ export default function List({ auth }: PageProps) {
 			header={
 				<div className="flex justify-between items-center flex-1 gap-6">
 					<h2 className="inline-flex gap-4 font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-						<FileChartColumn color="black" size={24} />
-						Reports
+						<GraduationCap color="black" size={24} />
+						Classes
 					</h2>
 				</div>
 			}
 		>
-			<Head title="Reports" />
+			<Head title="Classes" />
 			<div className="py-12">
 				<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-					<p>Reports</p>
+					<p>Classes</p>
 				</div>
 			</div>
 		</AuthenticatedLayout>
