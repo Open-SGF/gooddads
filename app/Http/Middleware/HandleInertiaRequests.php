fed0be87849b1ee37fc3cfdbe0d49ba744c1e7ea
Nathan Toombs <nathantoombs@Mac.attlocal.net>
2025-03-19 19:20:40 -0500

    Remove non-existent Ziggy class

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,42 +5,40 @@ namespace App\Http\Middleware;
 use App\Data\UserData;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
-use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
-  /**
-   * The root template that is loaded on the first page visit.
-   *
-   * @var string
-   */
-  protected $rootView = 'app';
+    /**
+     * The root template that is loaded on the first page visit.
+     *
+     * @var string
+     */
+    protected $rootView = 'app';
 
-  /**
-   * Determine the current asset version.
-   */
-  public function version(Request $request): ?string
-  {
-    return parent::version($request);
-  }
+    /**
+     * Determine the current asset version.
+     */
+    public function version(Request $request): ?string
+    {
+        return parent::version($request);
+    }
 
-  /**
-   * Define the props that are shared by default.
-   *
-   * @return array<string, mixed>
-   */
-  public function share(Request $request): array
-  {
-    return [
-      ...parent::share($request),
-      'auth.user' => fn() => $request->user()
-        ? UserData::from($request->user())
-        : null,
-      'ziggy' => fn() => [
-        ...(new Ziggy)->toArray(),
-        'location' => $request->url(),
-        'query' => $request->query() ?? [],
-      ],
-    ];
-  }
+    /**
+     * Define the props that are shared by default.
+     *
+     * @return array<string, mixed>
+     */
+    public function share(Request $request): array
+    {
+        return [
+            ...parent::share($request),
+            'auth.user' => fn () => $request->user()
+              ? UserData::from($request->user())
+              : null,
+            'ziggy' => fn () => [
+                'location' => $request->url(),
+                'query' => $request->query() ?? [],
+            ],
+        ];
+    }
 }
