db54c41fc9d1684cfb1b879ce2cbe3901c161b45
Nathan Toombs <ntoombs19@gmail.com>
2025-04-08 21:16:27 -0500

    Fix login store route

--- routes/auth.php
+++ routes/auth.php
@@ -17,8 +17,7 @@ Route::middleware('guest')->group(function () {
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
