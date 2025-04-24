54fc6c0d9926a860db423e0395de12a291b357be
Nathan Toombs <ntoombs19@gmail.com>
2025-04-24 00:12:29 -0500

    Fix default redirect for unauthenticated users

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        // For API requests, return null (will result in a 401 response)
+        if ($request->expectsJson()) {
+            return null;
+        }
+
+        // For web requests, redirect to home where the login page is rendered
+        return route('home');
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'auth' => \App\Http\Middleware\Authenticate::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
