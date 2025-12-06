094b7a1816a055806bb091cd9a2542c4dc96cbd7
Kayla Paden <kaylamarie1785@gmail.com>
2025-12-05 23:54:52 -0600

    fix: add fillable ID for Laravel mass assignment protection

--- app/Models/NeonHash.php
+++ app/Models/NeonHash.php
@@ -9,4 +9,8 @@ class NeonHash extends Model
     public $incrementing = false;
 
     protected $keyType = 'string';
+
+    protected $table = 'neon_participant_hashes'; 
+
+    protected $fillable = ['id'];
 }
